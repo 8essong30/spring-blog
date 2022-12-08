c2c2140092ac4833205ca8304c83ba78fb73dff8
SongeonLee <qkfka1040@icloud.com>
2022-12-08 21:36:58 +0900

    목록 조회 시 날짜 표시

--- src/main/java/com/sparta/blog/dto/BlogResponseDto.java
+++ src/main/java/com/sparta/blog/dto/BlogResponseDto.java
@@ -3,16 +3,22 @@ package com.sparta.blog.dto;
 import com.sparta.blog.entity.Blog;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class BlogResponseDto {
-    private String writer;
     private String title;
+    private String writer;
     private String contents;
+    private LocalDateTime modifiedAt;
+    private LocalDateTime createdAt;
 
     public BlogResponseDto(Blog blog) {
-        this.writer = blog.getWriter();
         this.title = blog.getTitle();
+        this.writer = blog.getWriter();
         this.contents = blog.getContents();
+        this.createdAt = blog.getCreatedAt();
+        this.modifiedAt = blog.getModifiedAt();
     }
 
 }
