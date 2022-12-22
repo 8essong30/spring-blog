f29451f322a6549f2478776ca736706e2e36ed79
SongeonLee <qkfka1040@icloud.com>
2022-12-23 01:04:00 +0900

    순환참조 에러 해결

--- src/main/java/com/sparta/blog/dto/BlogResponseDto.java
+++ src/main/java/com/sparta/blog/dto/BlogResponseDto.java
@@ -5,6 +5,7 @@ import com.sparta.blog.entity.Comment;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -16,7 +17,7 @@ public class BlogResponseDto {
     private LocalDateTime modifiedAt;
     private LocalDateTime createdAt;
 
-    private List<Comment> comments;
+    private List<CommentResponseDto> comments; // 여기서 본체가 나가면 안된다굿 entity가 밖으로 나가면 안돼
 
     public BlogResponseDto(Blog blog) {
         this.id = blog.getId();
@@ -25,6 +26,18 @@ public class BlogResponseDto {
         this.contents = blog.getContents();
         this.createdAt = blog.getCreatedAt();
         this.modifiedAt = blog.getModifiedAt();
+
+        List<CommentResponseDto> list = new ArrayList<>();
+        for (Comment comment : blog.getComments()) {
+            list.add(new CommentResponseDto(comment));
+        }
+        this.comments = list;
+
+/*
         this.comments = blog.getComments();
+        // getComments의 댓글리스트에서 Comment 클래스에 blog에 Getter가 붙어있어
+        // jackcon형님이 데이터를 json형식으로 변환해주려고 하는데 getter가 있어서 comment -> blog -> comment -> blog 반복
+        // dto 만들면 해결돼 안만들어서 그런거임
+*/
     }
 }
